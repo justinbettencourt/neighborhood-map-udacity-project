037de0edbc25bb578c178cce7e20070b64c30756
Justin Bettencourt <justinvbettencourt@gmail.com>
2017-01-31 12:46:47 -0500

    feat: InfoWindow now works and shows the streetview window for the location selected.

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ function initMap() {
 		coffeeShopLocations[i].marker = marker;
 
 		marker.addListener('click', function() {
-			// populateInfoWindow(this, infoWindow);
+			populateInfoWindow(this, infoWindow);
 		});
 	}
 	ko.applyBindings(new myViewModel());
@@ -57,6 +57,7 @@ var myViewModel = function() {
 		self.list.push(new listView(item));
 	});
 
+	// In place to setup the filter.
 	self.filter = ko.observable('');
 
 	// By using the ko.computed function, we can allow knockout.js to update itself every time the search has changed. The filter will come shortly once we get the list working as intended.
@@ -72,4 +73,48 @@ var myViewModel = function() {
 			return self.list();
         } 
     });
+}
+
+// This function gets called above when a marker is clicked. It'll display the information for the location selected inside of an InfoWindow.
+function populateInfoWindow(marker, infoWindow) {
+	// Check to make sure the infoWindow isn't already open for this marker.
+	if(infoWindow.marker != marker) {
+		infoWindow.setContent('');
+		infoWindow.marker = marker;
+		// Clear the marker property when the window closes.
+		infoWindow.addListener('closeClick', function() {
+			infoWindow.marker = null;
+		});
+
+		// Lets add the street view service to the project.
+		var streetViewService = new google.maps.StreetViewService();
+		var radius = 50;
+		
+		// Lets get the streetview data we need and create the contents inside the window.
+		function getStreetView(data, status) {
+			if(status == google.maps.StreetViewStatus.OK) {
+				var nearStreetViewLocation = data.location.latLng;
+				var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
+				infoWindow.setContent(
+						'<h3>' + marker.title + '</h3>' + 
+						'<div id="panorama"></div>'
+					);
+				var panoramaOptions = {
+					position: nearStreetViewLocation,
+					pov: {
+						heading: heading,
+						pitch: 0
+					}
+				};
+
+				var panorama = new google.maps.StreetViewPanorama(document.getElementById('panorama'), panoramaOptions);
+			} else {
+				infoWindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
+			}
+		}
+
+		streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+
+		infoWindow.open(map, marker);
+	}
 }
