9c9a0a68d99b402efced3554d13c4cfb0e63129a
Justin Bettencourt <justinvbettencourt@gmail.com>
2017-01-31 09:20:42 -0500

    feat: Items inside the observableArray now show up in the list view from items in the model.

--- js/main.js
+++ js/main.js
@@ -29,13 +29,47 @@ function initMap() {
 			animation: google.maps.Animation.DROP
 		});
 
-		markers.push(marker);
+		// For each marker in the for loop, set it as its own identity of the marker variable above.
+		coffeeShopLocations[i].marker = marker;
 
 		marker.addListener('click', function() {
 			// populateInfoWindow(this, infoWindow);
 		});
-
-		bounds.extend(markers[i].position);
 	}
+	ko.applyBindings(new myViewModel());
+}
 
+// Handles the data to be set into the observableArray.
+var listView = function(data) {
+	this.title = data.title;
+	this.location = data.location;
+	this.marker = data.marker;
+}
+
+/*** VIEW MODEL ***/
+var myViewModel = function() {
+	var self = this;
+
+	self.list = ko.observableArray([]);
+
+	// Push data in listView to observableArray "list".
+	coffeeShopLocations.forEach(function(item) {
+		self.list.push(new listView(item));
+	});
+
+	self.filter = ko.observable('');
+
+	// By using the ko.computed function, we can allow knockout.js to update itself every time the search has changed. The filter will come shortly once we get the list working as intended.
+	self.coffeeShopList = ko.computed(function(){
+        var filter = self.filter();
+        // If there is no filter, display all the contents on the list.
+        if(!filter){
+            self.list().forEach(function(item){
+            	if (item.marker == true) {
+                	item.marker.setVisible(true);
+                }
+            });
+			return self.list();
+        } 
+    });
 }
